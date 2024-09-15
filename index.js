f2e09e3ae62a7d0fdf670caf6505289ea01d79f6
andrew10966 <99299107+andrew10966@users.noreply.github.com>
2024-09-15 08:24:40 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('exploreButton');
-    button.addEventListener('click', () => {
-        alert('Exploring my creations!');
-        // Here you can add code to navigate to a different page or perform other actions.
-    });
-});
