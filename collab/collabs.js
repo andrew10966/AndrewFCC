ec64d01b59858c307365dfd006d857d27711a46d
andrew10966 <99299107+andrew10966@users.noreply.github.com>
2024-09-18 10:54:47 -0700

    Update collabs.js

--- collab/collabs.js
+++ collab/collabs.js
@@ -174,37 +174,61 @@ const trackIds = [
     { id: '4X7Ae3m7Ztenn6ssN0Xx4t', status: 'Open', genres: ['Hardcore'] },
     { id: '3tguTCjcPpazDMtmuyF8sy', status: 'Open', genres: ['Hardcore'] },
     { id: '2n7K5wAkx7uxMHngN4xZJw', status: 'Open', genres: ['Hardcore'] },
+    { id: '7dy6bWaPSLmipVoDoLaVeB', status: 'Open', genres: ['Electronic'] },
     { id: '1rxTtYlOQNVSm1kziQq39R', status: 'Open', genres: ['Hardcore'] },
     { id: '23BH0f8nTYV8KMryPScxdF', status: 'Open', genres: ['Hardcore'] },
     { id: '00pBUY9LKffZzeCH80JE0H', status: 'Open', genres: ['Hardcore'] },
+    { id: '5y5qNTyfvCQ6s230gLSjlv', status: 'Open', genres: ['Electronic'] },
     { id: '0QTwQbsSLkeFUGj8T6CgJV', status: 'Open', genres: ['Hardcore'] },
     { id: '1lZHiK2IInP32FeQFEp7ZV', status: 'Open', genres: ['Hardcore'] },
-    { id: '37IckXt4iECFcL0AsJP2Oa', status: 'Open', genres: ['Hardcore'] },
+    { id: '37IckXt4iECFcL0AsJP2Oa', status: 'Open', genres: ['Hardcore', 'Priority'] },
+    { id: '5WI2ltQIdwgzf1SNE76JyR', status: 'Open', genres: ['Electronic'] },
     { id: '0GLbugBl7oTlKZmlcm9M9w', status: 'Open', genres: ['Hardcore'] },
     { id: '03N15VIQOQVV2XcolaW0gR', status: 'Open', genres: ['Hardcore'] },
     { id: '7alnpbdgKsIvmvdBXtonFb', status: 'Open', genres: ['Hardcore'] },
     { id: '0c8luHrSaAMG072sEEpTii', status: 'Open', genres: ['Hardcore'] },
+    { id: '4ZFseyLstY6ZrXBzlXmuR0', status: 'Open', genres: ['Electronic'] },
     { id: '6LvC4nm62BQGESBPhwgpgg', status: 'Open', genres: ['Hardcore'] },
     { id: '1aFefpFxJjYLpeGGl4EELu', status: 'Open', genres: ['Hardcore'] },
-    { id: '2EHSBfC2epl3BXYR2uYrFd', status: 'Finished', genres: ['Hardcore'] },
+    { id: '2EHSBfC2epl3BXYR2uYrFd', status: 'Finished', genres: ['Hardcore', 'Priority'] },
     { id: '5sLoLqEFzmgq91nb6Q4cWv', status: 'Open', genres: ['Electronic'] },
     { id: '1EFPehJv13my9zvLFzGE8Z', status: 'Open', genres: ['Electronic'] },
     { id: '425KAU2QSkjYhTSDcgIHjO', status: 'Open', genres: ['Electronic'] },
     { id: '4TAeOsMYQ3IecAuljAF3Ft', status: 'Open', genres: ['Electronic'] },
     { id: '2ahW5IyuKcFCTnyV7gEvIC', status: 'Open', genres: ['Electronic'] },
+    { id: '1A8j067qyiNwQnZT0bzUpZ', status: 'Open', genres: ['Electronic'] },
     { id: '0Kz1564ihrYZNsgE76wDI0', status: 'Open', genres: ['Electronic'] },
     { id: '3RVKHMIsKak6QXsmu6i8ES', status: 'Open', genres: ['Electronic'] },
-    { id: '5y5qNTyfvCQ6s230gLSjlv', status: 'Open', genres: ['Electronic'] },
-    { id: '5WI2ltQIdwgzf1SNE76JyR', status: 'Open', genres: ['Electronic'] },
     { id: '0bHLNUoBzK83c61N9W4ohF', status: 'Open', genres: ['Electronic'] },
-    { id: '7dy6bWaPSLmipVoDoLaVeB', status: 'Open', genres: ['Electronic'] },
-    { id: '4ZFseyLstY6ZrXBzlXmuR0', status: 'Open', genres: ['Electronic'] },
     { id: '3XmNMIucTYuXOEpAjcaeVi', status: 'Open', genres: ['Electronic'] },
     { id: '3Vad9kkHv0DZZ0ioJdRrYV', status: 'Open', genres: ['Electronic'] },
     { id: '5kJ4BWZ9Y1qFIwwTbMIxYX', status: 'Open', genres: ['Electronic'] },
-    { id: '3nlwxpdoduF0VTIphyOS2Q', status: 'Open', genres: ['Electronic'] }
+    { id: '0lHAMNU8RGiIObScrsRgmP', status: 'Open', genres: ['Electronic'] },
+    { id: '3nlwxpdoduF0VTIphyOS2Q', status: 'Open', genres: ['Electronic', 'Priority'] },
+    { id: '2qG5sZ7Si6sdK74qLxedYM', status: 'Open', genres: ['Latin', 'Priority'] },
+    { id: '5OT9JgUd8WVWBJgYog7a82', status: 'Open', genres: ['Latin'] },
+    { id: '6Es8Sk3xe1HiJ2MXCfHHwR', status: 'Open', genres: ['Latin'] },
+    { id: '4UkUxO2WlKLc0Q1iEutGGh', status: 'Open', genres: ['Latin'] },
+    { id: '5lNcTbiGTEbPy6ZxmkXdLB', status: 'Open', genres: ['Latin'] },
+    { id: '5cAKmfCaqnYUnXqvTK8RxU', status: 'Open', genres: ['Latin'] },
+    { id: '2dYXrWkNO40NXy2Q3IYLRc', status: 'Open', genres: ['Latin'] },
+    { id: '7w7BrPbOjF5OxChs2dxFve', status: 'Open', genres: ['Latin'] },
+    { id: '3KKjc7pMPP3xgMNZ49Zm7R', status: 'Open', genres: ['Pop'] },
+    { id: '1RmakRsIJE6qsP1OjiUboJ', status: 'Open', genres: ['Hip-Hop'] },
+    { id: '0zO8ctW0UiuOefR87OeJOZ', status: 'Open', genres: ['Hip-Hop'] }
 ];
 const collabList = document.getElementById('collab-list');
+
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// Shuffle the trackIds array
+shuffleArray(trackIds);
 
 // Fetch data for each track and create a collab item for each one
 trackIds.forEach(async (track) => {
